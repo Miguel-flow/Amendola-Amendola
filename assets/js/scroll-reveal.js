288204e30ddcd41b1b1fc9b6ea678ecfd845e5aa
Miguel-flow <miguel@amendolaeamendola.com.br>
2026-05-26 16:28:50 -0300

    aaaa

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('sr-visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.12,
+    rootMargin: '0px 0px -48px 0px'
+  });
+
+  function init() {
+    document.querySelectorAll('[data-sr]').forEach(function (el) {
+      observer.observe(el);
+    });
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
